1b03a99952320df4fc80842a9cbd84f99fb4a384
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-05-24 11:40:25 +0200

    Add postman tests

--- server/controllers/camels.js
+++ server/controllers/camels.js
@@ -4,9 +4,9 @@ var Camel = require('../models/camel');
 
 router.get('/', function (req, res) {
     res.json({ "camels": [
-        { "color": "orange", "position": 1 },
-        { "color": "blue", "position": 2 },
-        { "color": "green", "position": 3 }
+        { "_id": 0, "color": "orange", "position": 1 },
+        { "_id": 1, "color": "blue", "position": 2 },
+        { "_id": 2, "color": "green", "position": 3 }
     ] });
 })
 
