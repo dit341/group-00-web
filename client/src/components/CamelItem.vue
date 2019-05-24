28071ec4e8d1dc97266e733c92f4c41e523bd6e4
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-05-24 16:44:16 +0200

    Add dynamic SVG icon support

--- client/src/components/CamelItem.vue
+++ client/src/components/CamelItem.vue
@@ -1,11 +1,21 @@
 <template>
-  <b-list-group-item :style="{ color: camel.color }">{{ camel.color }} : {{ camel.position }}</b-list-group-item>
+  <b-list-group-item :style="{ color: camel.color }">
+    <svg-icon size="20" viewBox="0 0 552.83 552.83" :color="camel.color"><camel-icon /></svg-icon>
+    {{ camel.color }} : {{ camel.position }}
+  </b-list-group-item>
 </template>
 
 <script>
+import SvgIcon from '@/components/SvgIcon'
+import CamelIcon from '@/components/icons/CamelIcon'
+
 export default {
   name: 'camel-item',
-  props: ['camel']
+  props: ['camel'],
+  components: {
+      SvgIcon,
+      CamelIcon
+  }
 }
 </script>
 
---
+++ client/src/components/SvgIcon.vue
@@ -0,0 +1,65 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" 
+    :width="getWidth" 
+    :height="getHeight" 
+    :viewBox="getViewBox" 
+    :aria-labelledby="name" 
+    role="presentation"
+  >
+    <title :id="name" lang="en">{{name}} icon</title>
+    <g :fill="color">
+      <slot />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'svg-icon',
+  props: {
+    name: {
+      type: String,
+      default: 'camel'
+    },
+    width: {
+      type: [Number, String],
+      default: undefined
+    },
+    height: {
+      type: [Number, String],
+      default: undefined
+    },
+    size: {
+      type: [Number, String],
+      default: 18
+    },
+    color: {
+      type: String,
+      default: 'black' // 'currentColor' for inherited
+    },
+    viewBox: {
+      type: String,
+      default: undefined
+    }
+  },
+  computed: {
+    getWidth () {
+      return this.width || this.size
+    },
+    getHeight () {
+      return this.height || this.size
+    },
+    getViewBox () {
+      return this.viewBox || `0 0 ${this.width} ${this.height}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+svg {
+  display: inline-block;
+  vertical-align: baseline;
+  margin-bottom: -2px;
+}
+</style>
