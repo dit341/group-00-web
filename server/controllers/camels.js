7c7f86d6ce6d991bdef6eeec0bb05bde642be8a6
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-05-25 16:40:34 +0200

    Add server debug config

--- server/controllers/camels.js
+++ server/controllers/camels.js
@@ -3,11 +3,13 @@ var router = express.Router();
 // var Camel = require('../models/camel');
 
 router.get('/', function (req, res) {
-    res.json({ 'camels': [
+    var camels = { 'camels': [
         { '_id': 0, 'color': 'orange', 'position': 1 },
         { '_id': 1, 'color': 'blue', 'position': 2 },
-        { '_id': 2, 'color': 'green', 'position': 3 }
-    ] });
+        { '_id': 2, 'color': 'green', 'position': 3 },
+        { '_id': 3, 'color': 'yellow', 'position': 4 },
+    ] };
+    res.json(camels);
 });
 
 router.delete('/:id', function (req, res) {
