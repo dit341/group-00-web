8beda41381f3083c6fddebfae94c543c15a00d57
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-05-26 21:43:39 +0200

    Handle mongodb connection error for test cleaner script

--- server/tests/dropdb.js
+++ server/tests/dropdb.js
@@ -4,7 +4,12 @@ var mongoose = require('mongoose');
 var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/server_test';
 
 // Drop database
-mongoose.connect(mongoURI, { useNewUrlParser: true }, function () {
+mongoose.connect(mongoURI, { useNewUrlParser: true }, function (err) {
+    if (err) {
+        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+        console.error(err.stack);
+        process.exit(1);
+    }
     mongoose.connection.db.dropDatabase(function () {
         console.log(`Dropped database: ${mongoURI}`);
         process.exit(0);
