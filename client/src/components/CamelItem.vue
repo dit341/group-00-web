da1aa9eec3476d9f9f16ec031ebc487388d6aee0
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-06-19 13:57:25 +0200

    Remove BaseIcon and CamelIcon setup
    + BaseIcon provides a nice way to include dynamic SVG images
    - Too complicated (too much overhead) for this minimal template

--- client/src/components/CamelItem.vue
+++ client/src/components/CamelItem.vue
@@ -1,25 +1,16 @@
 <template>
   <b-list-group-item :style="{ color: camel.color }">
     <img alt="Camel" src="../assets/camel.jpg">
-    <svg-icon size="20" viewBox="0 0 552.83 552.83" :color="camel.color">
-      <camel-icon/>
-    </svg-icon>
     {{ camel.color }} is at position {{ camel.position }}
     <b-button type="button" class="close" @click="$emit('delete-camel', camel._id)">&times;</b-button>
   </b-list-group-item>
 </template>
 
 <script>
-import SvgIcon from '@/components/SvgIcon'
-import CamelIcon from '@/components/icons/CamelIcon'
 
 export default {
   name: 'camel-item',
-  props: ['camel'],
-  components: {
-    SvgIcon,
-    CamelIcon
-  }
+  props: ['camel']
 }
 </script>
 
