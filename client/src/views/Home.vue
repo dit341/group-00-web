93396fa4cc208176b903487d43115be7b01ef152
Joel Scheuner <joel.scheuner.dev@gmail.com>
2019-07-03 12:50:46 +0200

    Add responsive demo with BootstrapVue
    Using its Layout and Grid System: https://bootstrap-vue.js.org/docs/components/layout/

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,6 +4,17 @@
       <p>Message from the server: {{ message }}</p>
       <b-button variant="primary" href="/camels">List Camels</b-button>
     </b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col><h1>Responsive BootstrapVue: <a href="https://bootstrap-vue.js.org/docs/components/layout/">Layout and Grid System</a></h1></b-col>
+      </b-row>
+      <!-- Mobile (< 768px): stack columns by making one half-width and one full-width
+           Desktop (>= 768px): keep both columns in same row in 4:8 ratio -->
+      <b-row>
+        <b-col class="example-col" cols="6" md="4">Message from the server</b-col>
+        <b-col class="example-col" cols="12" md="8">{{ message }}</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -34,3 +45,12 @@ export default {
   }
 }
 </script>
+
+<style>
+.example-col {
+  padding-top: .75rem;
+  padding-bottom: .75rem;
+  background-color: whitesmoke;
+  border: 2px solid lightgray;
+}
+</style>
